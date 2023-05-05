22516cb61b43e43871e163027e44e99943c2ff88
racoelhosilva <racoelhosilva@gmail.com>
2023-05-05 20:26:57 +0100

    ALL TEST FINISHED!!!!!!!!!!!!!!

--- XPM2.cpp
+++ XPM2.cpp
@@ -33,10 +33,14 @@ namespace prog {
             fileReader >> symbol; 
             fileReader >> generalInput;
             fileReader >> hexcode;
-            // Color currentColor = hex_to_dec(hexcode);
-            indexSymbolColor.insert({symbol, Color()});
-            // Creates and empty color *fix later*
-            // std::cout << symbol << " " << hexcode << "\n";
+
+            std::string red_hex = (hexcode.substr(1, 2));
+            std::string green_hex = (hexcode.substr(3, 2));
+            std::string blue_hex = (hexcode.substr(5, 2));
+
+            Color currentColor = Color(hex_to_dec(red_hex), hex_to_dec(green_hex),hex_to_dec(blue_hex));
+            indexSymbolColor.insert({symbol, currentColor});
+
         }
 
         // Loop through the nº of lines (dimension)
@@ -47,7 +51,6 @@ namespace prog {
         for (int lineIndex = 0; lineIndex < height; lineIndex++){
             std::string currentLine;
             std::getline (fileReader, currentLine);
-            // std::cout << currentLine << "\n";
             for (int colIndex = 0; colIndex < width; colIndex++){
                 char symbolRead = currentLine.at(colIndex);
                 Color currentColor = indexSymbolColor[symbolRead];
@@ -58,46 +61,26 @@ namespace prog {
         return resultImage;
     }
 
-    std::string lowercase(const std::string& word){
-        std::string result;
-        for (int idx = 0; idx < (int)word.size(); idx++){
-            result.push_back(tolower((char)word[idx]));
+    rgb_value hex_to_dec(std::string& hex_value){
+        rgb_value result = 0;
+        for (char symbol : hex_value){
+            result *= 16;
+            switch (symbol){
+            case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+                result += (symbol - '0'); break;
+            case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+                result += (symbol - 'a' + 10); break;
+            case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+                result += (symbol - 'A' + 10); break;
+            default:
+                result += 0; break;
+            }
         }
         return result;
     }
 
-    Color hex_to_dec(std::string hexcode){
-        std::string hexlower = lowercase(hexcode);
-        std::string red = hexlower.substr(1, 1+2);
-        std::string green = hexlower.substr(3, 3+2);
-        std::string blue = hexlower.substr(5, 5+2);
-        int red_ = 0, blue_ = 0, green_ = 0; int sixteen = 16; int temp = 0;
-        for (char i : red){
-            temp += (i >= 'A') ? (i >= 'a') ? (i - 'a' + 10) : (i - 'A' + 10) : (i - '0');
-            red_ += sixteen * temp;
-            sixteen = 1; }
-        sixteen = 16; temp = 0;
-        for (char i : green){
-            temp += (i >= 'A') ? (i >= 'a') ? (i - 'a' + 10) : (i - 'A' + 10) : (i - '0');
-            green_ += sixteen * temp;
-            sixteen = 1; }
-        sixteen = 16; temp = 0;
-        for (char i : blue){
-            temp += (i >= 'A') ? (i >= 'a') ? (i - 'a' + 10) : (i - 'A' + 10) : (i - '0');
-            blue_ += sixteen * temp;
-            sixteen = 1;
-        }
-        return Color(red_, green_, blue_);
-    }
+//===========================================================//
 
-
-
-    bool operator<(const Color& c1, const Color& c2){
-        bool red_check = c1.red() < c2.red();
-        bool green_check =  c1.red() == c2.red() && c1.green() < c2.green();
-        bool blue_check = c1.red() == c2.red() && c1.green() == c2.green() && c1.blue() < c2.blue();
-        return red_check || green_check || blue_check;
-    }
 
     void saveToXPM2(const std::string& file, const Image* image) {
         // Generate a map of color to symbol
@@ -122,9 +105,12 @@ namespace prog {
         fileWriter << image->width() << ' ' << image->height() << ' ' << symbolsCounter << " 1" << '\n';
 
         // Create the index for the colors
-        for (const auto& indexItr : indexColorSymbol){
-            // std::string hexcode = (indexItr.first);
-            fileWriter << indexItr.second << " c " << "#HEXCODE" << '\n';
+        for (auto& indexItr : indexColorSymbol){
+            std::string red_hex = dec_to_hex(indexItr.first.red());
+            std::string green_hex = dec_to_hex(indexItr.first.green());
+            std::string blue_hex = dec_to_hex(indexItr.first.blue());
+
+            fileWriter << indexItr.second << " c #" << red_hex << green_hex << blue_hex << '\n';
         }
         // Loop through each line
             // Loop through each color
@@ -141,5 +127,34 @@ namespace prog {
             fileWriter << '\n';
         }
         // This can be improved if we append strings over and over and only send to the file in the end
+    }
+
+    std::string dec_to_hex(rgb_value c){
+        std::string result = "";
+        rgb_value first = c / 16;
+        if (first >= 10){
+            result.push_back(char(first - 10 + 'A'));
+        }
+        else {
+            result.push_back(char(first + '0'));
+        }
+        
+        rgb_value second = c % 16;
+        if (second >= 10){
+            result.push_back(char(second - 10 + 'A'));
+        }
+        else {
+            result.push_back(char(second + '0'));
+        }
+        return result;
+    }
+
+
+
+    bool operator<(const Color& c1, const Color& c2){
+        bool red_check = c1.red() < c2.red();
+        bool green_check =  c1.red() == c2.red() && c1.green() < c2.green();
+        bool blue_check = c1.red() == c2.red() && c1.green() == c2.green() && c1.blue() < c2.blue();
+        return red_check || green_check || blue_check;
     }
 }
--- XPM2.hpp
+++ XPM2.hpp
@@ -11,12 +11,16 @@
 
 namespace prog {
     Image* loadFromXPM2(const std::string &file);
+    // Helper function for load
+    rgb_value hex_to_dec(std::string& hex_value);
 
     void saveToXPM2(const std::string& file, const Image* img);
 
+    // Helper function for save
     bool operator<(const Color& c1, const Color& c2);
+    std::string dec_to_hex(rgb_value c);
 
-    std::string lowercase(const std::string& word);
+    
 
 }
 #endif
