b4427da72e99ddcfb7472108741718f558d0276a
racoelhosilva <racoelhosilva@gmail.com>
2023-05-06 11:10:07 +0100

    Started Commenting Phase (XPM2)

--- XPM2.hpp
+++ XPM2.hpp
@@ -1,3 +1,21 @@
+/*  File: XPM2.cpp
+
+    ██████╗ ██████╗  ██████╗  ██████╗ 
+    ██╔══██╗██╔══██╗██╔═══██╗██╔════╝ 
+    ██████╔╝██████╔╝██║   ██║██║  ███╗
+    ██╔═══╝ ██╔══██╗██║   ██║██║   ██║
+    ██║     ██║  ██║╚██████╔╝╚██████╔╝
+    ╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ 
+                                  
+Group:
+    - 13.10
+Students:
+    - Gonçalo Morais Magalhães
+    - Rodrigo Albergaria Coelho e Silva
+    - Tiago Morais de Pimentel Teixeira
+
+Tip: the implementation of the functions on the .cpp files are in the same order as they appear defined in the corresponding .hpp header file
+*/
 #ifndef __prog_XPM2_hpp__
 #define __prog_XPM2_hpp__
 
@@ -12,13 +30,12 @@
 namespace prog {
     // Load an image from XPM2 file
     Image* loadFromXPM2(const std::string &file);
-    // Helper function for load
+    // Helper function for loadFromXPM2
     rgb_value hex_to_dec(std::string& hex_value);
-
 
     // Save an image into XPM2 file
     void saveToXPM2(const std::string& file, const Image* img);
-    // Helper functions for save
+    // Helper functions for saveToXPM2
     bool operator<(const Color& c1, const Color& c2);
     std::string dec_to_hex(rgb_value c);
 }
